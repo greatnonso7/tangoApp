f888d09c877bb810d64fa6ac51477ddf232cc5e3
Ichoku Chinonso <greatchinonso7@gmail.com>
2021-06-11 09:22:24 +0100

    initial set up for tango application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      houses: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://anapioficeandfire.com/api/houses').then((res) => {
+      this.setState({ houses: res.data });
+    });
+  }
+
+  render() {
+    const { houses } = this.state;
+    console.log(houses);
+    return (
+      <div className="App">
+        <h1>Hello world!</h1>
+        {houses.map((house) => (
+          <h3 key={house.name}>{house.name}</h3>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
